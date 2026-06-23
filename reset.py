47af638c8c30a1966e5c0a5ecf071f500b0b5fe6
Coldqueen19 <zachary@dentrassi.net>
2026-06-23 16:34:14 +0100

    .

--- reset.py
+++ reset.py
@@ -1,8 +1,8 @@
 import pickle
-def reset():
-    completed_starter = False
-    cave = "goatchurch"
-    money = 100
-    caves = []
-    with open('savefile.pkl', 'wb') as file:
-        pickle.dump([completed_starter, cave, money, caves], file)
+completed_starter = False
+cave = "training"
+money = 0
+caves = []
+starttext = "MANDATORY ORIGINAL TRAINING"
+with open('savefile.pkl', 'wb') as file:
+    pickle.dump([completed_starter, cave, money, caves, starttext], file)
